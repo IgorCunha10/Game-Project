2dbadd0bd5304b63dd486ac45bfac7db69fe6610
Igor Cunha <[200~git config --global user.email>
2026-02-15 12:52:43 -0300

    Created the game window using JFrame in JavaGame.java and JPanel in GamePanel.java.

--- project.java.game/src/JavaGame.java
+++ project.java.game/src/JavaGame.java
@@ -1,3 +1,29 @@
+import javax.swing.*;
+
 public class JavaGame {
+
+    public static void main(String args[]) {
+
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Java Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+
+    }
+
+
+
+
 
 }
