c6a3381bbcefa7c7dc21ed8c8ac64158a61835e4
Igor Cunha <gigor497@gmail.com>
2026-06-13 12:03:06 -0300

    Implemented Camera Tracking, New Map Sprites and New Map.

--- project.java.game/src/main/GamePanel.java
+++ project.java.game/src/main/GamePanel.java
@@ -17,12 +17,19 @@ public class GamePanel extends JPanel implements Runnable {
     public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
+
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeigh = tileSize * maxWorldRow;
+
     int FPS = 60;
+
 
     TileManager tileManager = new TileManager(this);
     Thread gameThread;
     KeyHandler keyH = new KeyHandler();
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
     public GamePanel() {
 
